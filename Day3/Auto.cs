f002d343211484666326a2d02f62c6a3219be881
narmada <narmada@kth.se>
2016-06-28 09:40:45 +0200

    Day3

---
+++ Day3/Auto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Auto
+    {
+        private int speed;
+        public int Year;
+        public int Miles;
+        private string make;
+        public string Model;
+        public string Color;
+
+        public string Maker
+        {
+            get { return make; }
+            set
+            {
+                switch (value)
+                {
+                    case "oldsMobile":
+                        make = value;
+                        break;
+                    case "volvo":
+                        make = value;
+                        break;
+                    default:
+                        throw new Exception  ("invalid Maker");
+                        
+                }
+               
+            }
+        }
+        public int currentSpeed
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 0)
+                    speed = 0;
+                else if (value > 10)
+                    speed = 10;
+                else
+                    speed = value;
+            }
+        }
+
+
+        public int Accelerator(int incrementedSpeed)
+        {
+            currentSpeed += incrementedSpeed;
+            //Console.WriteLine("the current speed:" + speed);
+            return currentSpeed;
+
+        }
+        public int Decelerator(int decrementedSpeed)
+        {
+            currentSpeed -= decrementedSpeed;
+            //Console.WriteLine("The current speed:" +speed);
+            return currentSpeed;
+        }
+        public string speedLimitViolation(int speedLimit, string initialMessage)
+        {
+            string message = "";
+            if (currentSpeed > speedLimit)
+                // initialMessage = "warning!";
+                message = initialMessage + "Too Fast!";
+            else
+                message = "No worries !! It's ok!";
+            //initialMessage = "No worries!";
+            return message;
+
+        }
+
+
+        }
+    }
---
+++ Day3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto myCar = new Auto();
+            myCar.currentSpeed = 0;
+            myCar.Model = "SwiftDezire";
+            myCar.Year = 2009;
+            myCar.Maker = "volvo";
+            myCar.Miles = 50000;
+            myCar.Color = "Silver";
+            Console.WriteLine(myCar.Maker + "  " + myCar.Model);
+
+            int myCurrentSpeeed = 0;
+            myCurrentSpeeed = myCar.Accelerator(3);
+            myCar.Accelerator(1);
+            myCar.Accelerator(2);
+            myCar.Accelerator(1);
+            myCurrentSpeeed = myCar.Accelerator(2);
+            Console.WriteLine("The Current speed of the car is: " + myCurrentSpeeed);
+
+            string message = myCar.speedLimitViolation(10, "Warning!");
+
+            Console.Beep();
+
+            Console.WriteLine(message);
+            //Console.WriteLine(message1);
+            myCurrentSpeeed = myCar.Decelerator(10);
+            Console.WriteLine("The reduced  speed of the car is: " + myCurrentSpeeed);
+
+            Console.ReadLine();
+        }
+    }
+}
